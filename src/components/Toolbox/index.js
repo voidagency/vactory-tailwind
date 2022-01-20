df999e28df0d0a6ca0ac99be3c3a82d7fa845649
Abdelkoddous <a.elidrissi@void.fr>
2022-01-20 17:15:41 +0100

    Add Toolbox

---
+++ src/components/Toolbox/index.js
@@ -0,0 +1,4 @@
+import { IconsOnly, Sliding, IconAndLabel, Grouped } from "./Toolbox.stories";
+
+export { IconsOnly, Sliding, IconAndLabel, Grouped };
+export default { IconsOnly, Sliding, IconAndLabel, Grouped };
